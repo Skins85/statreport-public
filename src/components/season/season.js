6c71d78de9bb9ddb569ced0e140d0c60765e1b89
Mark Skinsley <mark.skinsley@gmail.com>
2021-05-26 17:03:23 +0100

    1. useAxios hook 2. Start/sub count helper functions

--- src/components/season/season.js
+++ src/components/season/season.js
@@ -1,55 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import {playerStartsFilter, playerSubsFilter} from '../../util';
 
 import Spinner from '../ui/spinner/spinner';
 import Table from '../hoc/table/table';
-import axios from 'axios';
-import { nameFormat } from '../../util';
-import { setupCache } from 'axios-cache-adapter';
+import useAxios from '../../hooks/useAxios';
 
 export default function Season() {
 
-    const [data, setData] = useState(false);
-    const [hasError, setErrors] = useState(false);
-    const [dataLoaded, setDataLoaded] = useState(false);
+    // State
+    const { data: matchesData, hasError: matchesError, dataLoaded: matchesDataLoaded } = useAxios('https://www.statreport.co.uk/api/json/data-matches.php');
+    const { data: playersData, hasError: playersError, dataLoaded: playersDataLoaded } = useAxios('https://www.statreport.co.uk/api/json/data-players.php');
 
-    useEffect(() => {
-        async function fetchData() {
-            
-            // Cache GET requests
-            let cache;
-            function cacheReq() {
-                // Define cache adapter and manage properties
-                cache = setupCache({
-                    maxAge: 15 * 60 * 1000
-                })
-            }
-            await cacheReq(cache);
+    // Variables
+    let matches = matchesData.results;
 
-            // Create cache adapter instance
-            const api = axios.create({
-                adapter: cache.adapter
-            })
-        
-            // Cache GET responses and save in state
-            await api({
-                url: 'https://www.statreport.co.uk/api/json/data-matches.php',
-                method: 'get'
-            }).then(async (response) => {
-                setData(response.data);
-            })
+    if (matches) {
+        matches = matches.filter((match) => match.season === '2020-21');
+        let playerStarts = playerStartsFilter(matches, 'justham-elliot');
+        let playerSubs = playerSubsFilter(matches, 'balanta-angelo');
 
-            await setDataLoaded(true);
-        }
-        fetchData();
-    },[]);
+        console.log(playerStarts);
+        console.log(playerSubs);
+    }
 
-    let matches = data.results;
-    console.log(matches);
-
-    function sum(num1, num2) => num1 + num2;
+    
 
     return (
-        <p>Season</p>
+        <React.Fragment>
+            <p>data</p>
+        </React.Fragment>
     )
 
 }
