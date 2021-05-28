a6f7abc66d3fe2f2668509e01c79681fcee86700
Mark Skinsley <mark.skinsley@gmail.com>
2021-05-28 10:50:26 +0100

    Filter object data by array of keys

--- src/components/season/season.js
+++ src/components/season/season.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {playerStartsFilter, playerSubsFilter, removeEmptyObjectValues} from '../../util';
+import {playerStartsFilter, playerStartsFilterNew, playerSubsFilter, removeEmptyObjectValues} from '../../util';
 
 import Spinner from '../ui/spinner/spinner';
 import Table from '../hoc/table/table';
@@ -15,6 +15,11 @@ export default function Season() {
     let matches = matchesData.results,
         players = playersData.results,
         playerInfoAll = []; // Send to appearances table component
+
+    const startsFull = ['player_1', 'player_2', 'player_3', 'player_4', 'player_5', 'player_6', 'player_7', 'player_8', 'player_9', 'player_10', 'player_11'];
+    const starts = ['player_1', 'player_2'];
+    let a = playerStartsFilterNew(matches, starts, 'justham-elliot');
+    console.log(a);
 
     if (matches && players) {
         matches = matches.filter((match) => match.season === '2020-21');
