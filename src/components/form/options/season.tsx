1b089060ba2233e344e65f1cab32140c9afd7cc9
Mark Skinsley <mark.skinsley@gmail.com>
2022-06-26 11:58:20 +0100

    Add 2011-12 to season select

--- src/components/form/options/season.tsx
+++ src/components/form/options/season.tsx
@@ -13,6 +13,7 @@ const seasonOptions = () => {
             <option key='2014-15' value='2014-15' name='2014-15'>2014-15</option>
             <option key='2013-14' value='2013-14' name='2013-14'>2013-14</option>
             <option key='2012-13' value='2012-13' name='2012-13'>2012-13</option>
+            <option key='2011-12' value='2011-12' name='2011-12'>2011-12</option>
         </>
     )    
 }
